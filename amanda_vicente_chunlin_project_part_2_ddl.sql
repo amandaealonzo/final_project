4381c7f6b73d95522637fb5e3ce9a940064cda6f
amandaealonzo <amanda.e.alonzo@gmail.com>
2022-07-02 11:10:05 -0700

    Add files via upload

--- amanda_vicente_chunlin_project_part_2_ddl.sql
+++ amanda_vicente_chunlin_project_part_2_ddl.sql
@@ -1,6 +1,6 @@
+-- Amanda
+
 create database project
-
-
 
 -- user table (for CRUD) activities
 CREATE TABLE IF NOT EXISTS users (
@@ -122,9 +122,9 @@ CREATE  TABLE host (
   `longitude` double DEFAULT NULL
   );
 
-
+drop table specification;
 create table specification (
- specification_id int primary key,
+ specification_id int auto_increment primary key,
  listing_id int,
  `room_type` text,
   `accommodates` int(11) DEFAULT NULL,
@@ -147,9 +147,6 @@ create table review (
 );
 
 delete from host;
-select * from host;
-DESCRIBE HOST
-describe listing
 
 insert into host (
 select distinct host_id, 
@@ -161,11 +158,12 @@ select distinct host_id,
  host_response_rate, host_acceptance_rate, host_is_superhost, host_thumbnail_url,
  host_picture_url, host_neighbourhood, host_listings_count, host_total_listings_count, host_verifications, 
  host_has_profile_pic, host_identity_verified
-from listing );
+from listing_raw  where host_id <> 2206506 or host_since not like '%/1914%'); -- data cleansing for one host with duplicate rows
 commit;
 
 select count(*) from host where host_id ='2206506'
- drop table listing;
+
+drop table listing;
  
 Create table listing (
 Listing_id int primary key,
@@ -191,25 +189,111 @@ delete from location;
 insert into location ( neighbourhood_group_cleansed, neighbourhood_cleansed, latitude, longitude)
 select distinct neighbourhood_group_cleansed, neighbourhood_cleansed, latitude, longitude from listing_raw;
 commit;
-
-select * from location order by 2;
  
+create unique index location_ix on location(latitude, longitude);
+create   index location_ix on listing_raw(latitude, longitude);
 
-insert into listing (  listing_id, listing_url, price, minimum_nights, maximum_nights, availability_30, availability_60, availability_90, availability_365, number_of_review, first_review, last_review)
-select   id, listing_url,  price, minimum_nights, maximum_nights, availability_30, availability_60, availability_90, availability_365, number_of_reviews, first_review, last_review
-from listing_raw;
+describe listing;
+
+insert into listing (  listing_id, location_id, host_id, listing_url, price, minimum_nights, maximum_nights, availability_30, availability_60, availability_90, availability_365, number_of_review, first_review, last_review)
+select distinct   id,  b.location_id, host_id,  listing_url,  replace(price,'%','') , minimum_nights, maximum_nights, availability_30, availability_60, availability_90, availability_365, number_of_reviews, first_review, last_review
+from 
+listing_raw a
+left outer join location b on a.latitude = b.latitude and a.longitude = b.longitude;
+
+commit;
+ 
+ describe specification
+ 
+insert into specification (  listing_id,
+room_type,
+bathrooms,
+bathrooms_text,
+bedrooms,
+beds)
+select distinct   id,  
+room_type,  bathrooms, bathrooms_text, bedrooms, beds
+from 
+listing_raw;
+
 commit;
 
-update listing a set location_id = (select location_id from location b where a.latitude = b.latitude and a.longitude = b.longitude);
+describe listing
+ 
+delete  from review_date where listing_id not in (select listing_id from listing);
+commit;
 
+delete  from review_date where listing_id is null;
+commit;
 
 alter table review add constraint fk_listing   foreign key (listing_id) references listing (listing_id);
 
-alter table specification add constraint fk_listing   foreign key (listing_id) references listing (listing_id);
+alter table specification add constraint fk_listing2   foreign key (listing_id) references listing (listing_id);
 
-alter table review_date add constraint fk_listing   foreign key (listing_id) references listing (listing_id);
+alter table review_date add constraint fk_listing3   foreign key (listing_id) references listing (listing_id);
 
 alter table listing add constraint fk_location   foreign key (location_id) references location (location_id);
 
 alter table listing add constraint fk_host   foreign key (host_id) references host (host_id);
+
+-- SAMPLE QUERIES FOR APPLICATION
+
+-- neighborhood monthly summary
+
+describe review_date
+describe location
+
+create index rev_listing_ix on review_date (listing_id);
+create index listing_loc_ix on listing (location_id);
+
+-- check monthly volume over time by  neighborhood
+
+select year(date) || '-' || month(date) month_cd, neighbourhood_group_cleansed, count(date) review_count
+from
+listing a
+left outer join review_date b on a.listing_id = b.listing_id
+left outer join location c on a.location_id = c.location_id
+group by  year(date) || '-' || month(date) , neighbourhood_group_cleansed
+order by 2,1;
+
+
+-- check long term rentals vs short term rentals  by  neighborhood
+
+select 
+sum(case when minimum_nights > 30 then 1 else  0 end) long_term_count, count(*) total_count,  
+sum(case when minimum_nights > 30 then 1 else  0 end)/count(*) * 100 long_term_percent,
+neighbourhood_group_cleansed ,
+neighbourhood_cleansed
+from
+listing a
+left outer join location c on a.location_id = c.location_id
+ group by neighbourhood_group_cleansed, neighbourhood_cleansed
+order by 1 desc;
+
+/*
+Top  5 by percent:
+
+long total  percent
+1	2	50.0000	Staten Island	Rossville
+1	3	33.3333	Bronx	Spuyten Duyvil
+2	13	15.3846	Staten Island	Grant City
+5	37	13.5135	Manhattan	Roosevelt Island
+2	21	9.5238	Queens	Howard Beach
+
+Top 5 by volume:
+long total  percent
+41	787	    5.2097	Manhattan	East Village
+41	2078	1.9731	Brooklyn	Bedford-Stuyvesant
+38	1386	2.7417	Manhattan	Harlem
+37	1839	2.0120	Brooklyn	Williamsburg
+36	1123	3.2057	Brooklyn	Bushwick
+34	984	    3.4553	Manhattan	Midtown
+29	616	    4.7078	Manhattan	Chelsea
+
+*/
+
+
+
+describe listing
+
 
